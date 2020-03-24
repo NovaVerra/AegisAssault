9617e6d8a84a3204ede1b89d192ee3e9c31cb09e
lununglee <leo.lunung.lee@gmail.com>
2020-03-23 18:52:32 -0700

    playing music between scenes with DontDestroyOnLoad()

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
+	/** Executed before "Start()" */
+	private void	Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+
 	// Start is called before the first frame update
-	void Start()
+	void			Start()
 	{
 		Invoke("LoadGame", 1f);
 	}
 
 	// Update is called once per frame
-	void Update()
+	void			Update()
 	{
 		
 	}
 
-	void	LoadGame()
+	void			LoadGame()
 	{
 		SceneManager.LoadScene(1);
 	}
