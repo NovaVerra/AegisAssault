cdfd2f0a6bc236e98c8d5477e2a5d88e288343f7
lununglee <leo.lunung.lee@gmail.com>
2020-03-24 11:47:55 -0700

    using cross platform input manager

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		float	HorizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+		print(HorizontalThrow);
+	}
+}
