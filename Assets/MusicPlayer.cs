e6576b8a59b28b49afcdcf235b859710ed96d5fd
lununglee <leo.lunung.lee@gmail.com>
2020-03-23 18:39:41 -0700

    flattened organization and added splash screen

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		Invoke("LoadGame", 1f);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	void	LoadGame()
+	{
+		SceneManager.LoadScene(1);
+	}
+}
