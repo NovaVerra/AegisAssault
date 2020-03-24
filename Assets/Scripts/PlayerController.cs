e5cfa71c7337c48eafd50b1c1a4a46555d742768
lununglee <leo.lunung.lee@gmail.com>
2020-03-24 16:27:33 -0700

    set local rotation through c#

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,27 @@ using UnityStandardAssets.CrossPlatformInput;
 public class PlayerController : MonoBehaviour
 {
 	/** Game Configuration */
-	[Tooltip("in ms^-1")][SerializeField] float	MovementSpeed = 4f;
-	[SerializeField] float	X_Range = 10f;
-	[SerializeField] float	Y_Range = 6.5f;
+	[Tooltip("in ms^-1")][SerializeField]
+	float	MovementSpeed = 4f;
+	[SerializeField]
+	float	X_Range = 10f;
+	[SerializeField]
+	float	Y_Range = 6.5f;
 
 	// Start is called before the first frame update
-	void Start()
+	void	Start()
 	{
-		
+
 	}
 
 	// Update is called once per frame
-	void Update()
+	void	Update()
+	{
+		ProcessTransform();
+		ProcessRotation();
+	}
+
+	void	ProcessTransform()
 	{
 		/** Read and bind user input */
 		float	X_Axis = CrossPlatformInputManager.GetAxis("Horizontal");
@@ -37,5 +46,10 @@ public class PlayerController : MonoBehaviour
 
 		/** Moving the ship */
 		transform.localPosition = new Vector3(X_NewVector, Y_NewVector, transform.localPosition.z);
+	}
+	
+	void	ProcessRotation()
+	{
+		transform.localRotation = Quaternion.Euler(-30f, 30f, 0f);
 	}
 }
