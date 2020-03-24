a87449cc266a878263adc4f18396573788fb31d0
lununglee <leo.lunung.lee@gmail.com>
2020-03-24 15:50:37 -0700

    clamped x & y values to keep ship within camera bounds

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ using UnityStandardAssets.CrossPlatformInput;
 public class PlayerController : MonoBehaviour
 {
 	/** Game Configuration */
-	[Tooltip("in ms^-1")][SerializeField] float	X_Speed = 4f;
+	[Tooltip("in ms^-1")][SerializeField] float	MovementSpeed = 4f;
+	[SerializeField] float	X_Range = 10f;
+	[SerializeField] float	Y_Range = 6.5f;
 
 	// Start is called before the first frame update
 	void Start()
@@ -17,8 +19,23 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		float	X_Throw = CrossPlatformInputManager.GetAxis("Horizontal");
-		float	Y_Throw = CrossPlatformInputManager.GetAxis("Vertical");
-		float	X_Offset_ThisFrame = X_Throw * X_Speed * Time.deltaTime;
+		/** Read and bind user input */
+		float	X_Axis = CrossPlatformInputManager.GetAxis("Horizontal");
+		float	Y_Axis = CrossPlatformInputManager.GetAxis("Vertical");
+	
+		/** Calculating offset */
+		float	X_Offset = X_Axis * MovementSpeed * Time.deltaTime;
+		float	Y_Offset = Y_Axis * MovementSpeed * Time.deltaTime;
+	
+		/** Calculating new position of ship */
+		float	X_NewVector = transform.localPosition.x + X_Offset;
+		float	Y_NewVector = transform.localPosition.y + Y_Offset;
+	
+		/** Restricting the movement within camera bounds */
+		X_NewVector = Mathf.Clamp(X_NewVector, -X_Range, X_Range);
+		Y_NewVector = Mathf.Clamp(Y_NewVector, -Y_Range, Y_Range);
+
+		/** Moving the ship */
+		transform.localPosition = new Vector3(X_NewVector, Y_NewVector, transform.localPosition.z);
 	}
 }
