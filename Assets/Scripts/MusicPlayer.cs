875c7300c285eb96d9b2d2689c5b80fdd1b61beb
lununglee <leo.lunung.lee@gmail.com>
2020-03-25 18:52:41 -0700

    modularizing code and reorganizing directory

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
@@ -9,22 +8,5 @@ public class MusicPlayer : MonoBehaviour
 	private void	Awake()
 	{
 		DontDestroyOnLoad(gameObject);
-	}
-
-	// Start is called before the first frame update
-	void			Start()
-	{
-		Invoke("LoadGame", 1f);
-	}
-
-	// Update is called once per frame
-	void			Update()
-	{
-		
-	}
-
-	void			LoadGame()
-	{
-		SceneManager.LoadScene(1);
 	}
 }
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		Invoke("LoadGame", 1f);
+	}
+
+	void			LoadGame()
+	{
+		SceneManager.LoadScene(1);
+	}
+}
