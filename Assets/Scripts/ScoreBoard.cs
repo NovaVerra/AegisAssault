d5868292dbeffa9a4518645152f5bb53468b9d44
lununglee <leo.lunung.lee@gmail.com>
2020-03-27 17:53:45 -0700

    initial setup for score board

---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+	/** Game Configuration */
+	[SerializeField] 
+	int		ScorePerHit = 12;
+	int		Score = 0;
+	Text	ScoreText;
+
+	// Start is called before the first frame update
+	void	Start()
+	{
+		ScoreText = GetComponent<Text>();
+		ScoreText.text = Score.ToString();
+	}
+
+	void	ScoreHit()
+	{
+		Score += ScorePerHit;
+		ScoreText.text = Score.ToString();
+	}
+}
